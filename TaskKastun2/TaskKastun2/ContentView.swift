44b13609fec0f29e06b727c4b80a9d79c8f4d5ba
sheegle <151909011+sheegle@users.noreply.github.com>
2023-11-27 16:05:33 +0900

    Update ContentView.swift

--- TaskKastun2/TaskKastun2/ContentView.swift
+++ TaskKastun2/TaskKastun2/ContentView.swift
@@ -7,35 +7,47 @@
 
 import SwiftUI
 
+struct TodoItem: Identifiable, Equatable {
+    var id = UUID()
+    var title: String
+    var completed: Bool
+}
+
+
 struct ContentView: View {
-    @State private var taskData = [(title: "", completed: false)]
+    @State private var taskData = [TodoItem(title: "Task 1", completed: false),
+                                   TodoItem(title: "Task 2", completed: false),]
     @State private var isShowAddView = false
     
     var body: some View {
         NavigationStack {
-            List(0..<taskData.count, id: \.self) { index in
-                Button {
-                    taskData[index].completed.toggle()
-                } label: {
-                    HStack {
-                        Image(systemName: taskData[index].completed ? "checkmark.circle.fill" :"circle" )
-                        Text(taskData[index].title)
+            List {
+                ForEach(taskData) { item in
+                    Button {
+                        taskData[taskData.firstIndex(of: item)!].completed.toggle()
+                    } label: {
+                        HStack {
+                            Image(systemName: item.completed ? "checkmark.circle.fill" :"circle" )
+                            Text(item.title)
+                        }
                     }
+                    .foregroundColor(.primary)
                 }
-                .foregroundColor(.primary)
-                
-                .navigationTitle("ToDoリスト")
-                .toolbar {
-                    Button("+") {
-                        isShowAddView = true
-                    }
+                .onDelete { indexSet in
+                    taskData.removeAll { indexSet.contains(taskData.firstIndex(of: $0)!) }
                 }
-                .sheet(isPresented: $isShowAddView){
-                    AddView(taskData: $taskData)
-                }
-                
             }
-            
+
+
+            .navigationTitle("ToDoリスト")
+            .toolbar {
+                Button("+") {
+                    isShowAddView = true
+                }
+            }
+            .sheet(isPresented: $isShowAddView){
+                AddView(taskData: $taskData)
+            }
         }
     }
 }
